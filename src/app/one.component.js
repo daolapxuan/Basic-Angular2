63c38a662e36b58f5781eb12dafa958e298e44da
DLX <daolapxuan@gmail.com>
2017-03-11 01:38:45 +0700

    Lesson11: Structural Directive

--- src/app/one.component.js
+++ src/app/one.component.js
@@ -1,4 +1,4 @@
-// Event Binding
+// Structural Directive
 "use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -14,23 +14,23 @@ var OneComponent = (function () {
     function OneComponent() {
         this.name = 'DLX';
         this.showIfLine = true;
+        this.color = 'blue';
+        this.colors = ["red", "green", "blue"];
     }
-    OneComponent.prototype.showIf = function (value) {
-        if (value == true) {
-            value = false;
-        }
-        if (value == false) {
-            value = true;
-        }
-    };
     OneComponent = __decorate([
         core_1.Component({
             selector: 'my-app1',
-            template: "\n    <h2>My name is {{name}}</h2>\n    <h3 *ngIf=\"showIfLine\" #show>This is ngIf Directive Line.</h3>\n    <button (click)=\"showIf(show)\">Show</button>\n    "
+            template: "\n    <h2>My name is {{name}}</h2>\n    <button>On/Off</button>\n    <h3 *ngIf=\"showIfLine\">This is ngIf Directive Line.</h3>\n    Import color: <input type=\"text\" #cl/>\n    <div [ngSwitch]=\"color\">\n        <p *ngSwitchCase=\"'red'\">This is color red</p>\n        <p *ngSwitchCase=\"'green'\">This is color green</p>\n        <p *ngSwitchCase=\"'blue'\">This is color blue</p>\n        <p *ngSwitchDefault>No color</p>\n    </div>\n    <p>Export elements of an array, with elements in class:</p>\n    <ul>\n        <li *ngFor=\"let color of colors\">{{color}}</li>\n    </ul>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], OneComponent);
     return OneComponent;
 }());
 exports.OneComponent = OneComponent;
+/*
+How to
+- Exercise 1: Create a button to on/off ngIf
+- Exercise 2: Create a input, when import color in that, it will show text
+- Exercise 3: Import elements of an array from keyboard and export that on screen
+ */ 
 //# sourceMappingURL=one.component.js.map
--- src/app/one.component.ts
+++ src/app/one.component.ts
@@ -1,4 +1,4 @@
-// Event Binding
+// Structural Directive
 
 import { Component } from '@angular/core';
 
@@ -6,8 +6,19 @@ import { Component } from '@angular/core';
     selector: 'my-app1',
     template: `
     <h2>My name is {{name}}</h2>
-    <h3 *ngIf="showIfLine" #show>This is ngIf Directive Line.</h3>
-    <button (click)="showIf(show)">Show</button>
+    <button>On/Off</button>
+    <h3 *ngIf="showIfLine">This is ngIf Directive Line.</h3>
+    Import color: <input type="text" #cl/>
+    <div [ngSwitch]="color">
+        <p *ngSwitchCase="'red'">This is color red</p>
+        <p *ngSwitchCase="'green'">This is color green</p>
+        <p *ngSwitchCase="'blue'">This is color blue</p>
+        <p *ngSwitchDefault>No color</p>
+    </div>
+    <p>Export elements of an array, with elements in class:</p>
+    <ul>
+        <li *ngFor="let color of colors">{{color}}</li>
+    </ul>
     `
 })
 
@@ -16,12 +27,14 @@ export class OneComponent {
 
     showIfLine = true;
 
-    showIf(value:boolean) {
-        if (value == true) {
-            value = false;
-        }
-        if (value == false) {
-            value = true;
-        }
-    }
-}    
+    color = 'blue';
+
+    colors: string[] = ["red", "green", "blue"];
+}
+
+/*
+How to 
+- Exercise 1: Create a button to on/off ngIf
+- Exercise 2: Create a input, when import color in that, it will show text
+- Exercise 3: Import elements of an array from keyboard and export that on screen
+ */
