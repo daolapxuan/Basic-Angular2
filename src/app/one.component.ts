0b04bc9ce4f688c7923b9c260367aed39a5beccb
DLX <daolapxuan@gmail.com>
2017-03-13 15:54:04 +0700

    lesson11: Structural Directive

--- src/app/one.component.ts
+++ src/app/one.component.ts
@@ -7,19 +7,26 @@ import { Component } from '@angular/core';
     template: `
     <h2>My name is {{name}}</h2>
     <button>On/Off</button>
-    <h3 *ngIf="showIfLine">This is ngIf Directive Line.</h3>
+    <h3>This is ngIf Directive</h3>
+    <h4 *ngIf="showIfLine">This is ngIf Directive Line.</h4>
+    <h3>this is ngSwitch Directive</h3>
     Import color: <input type="text" #cl/>
     <div [ngSwitch]="color">
-        <p *ngSwitchCase="'red'">This is color red</p>
-        <p *ngSwitchCase="'green'">This is color green</p>
-        <p *ngSwitchCase="'blue'">This is color blue</p>
+        <p style="color:red;" *ngSwitchCase="'red'">This is color red</p>
+        <p style="color:green;" *ngSwitchCase="'green'">This is color green</p>
+        <p style="color:blue;" *ngSwitchCase="'blue'">This is color blue</p>
         <p *ngSwitchDefault>No color</p>
     </div>
     <p>Export elements of an array, with elements in class:</p>
     <ul>
         <li *ngFor="let color of colors">{{color}}</li>
     </ul>
-    `
+    `,
+    styles: [`
+    h3 {
+        color: grey;
+    }
+    `]
 })
 
 export class OneComponent {
@@ -27,7 +34,7 @@ export class OneComponent {
 
     showIfLine = true;
 
-    color = 'blue';
+    color = 'red';
 
     colors: string[] = ["red", "green", "blue"];
 }
